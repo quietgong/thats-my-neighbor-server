50c279d81eadc2b1b48ca4f87048421cb5ec2976
quietgong <quietgong@naver.com>
2025-11-21 01:40:44 +0900

    server

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/api/locations", async (req, res) => {
 
         // 새 위치 저장
         const [result] = await connection.execute(
-            "INSERT INTO locations (user_id, latitude, longitude) VALUES (?, ?, ?, ?)",
+            "INSERT INTO locations (user_id, latitude, longitude, h) VALUES (?, ?, ?, ?)",
             [userId, lat, lng, h],
         )
 
